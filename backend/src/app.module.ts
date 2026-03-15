078af7f4b45b7a1d005afa42a1210c25dd66147e
danielarjordao <danielarjordao@hotmail.com>
2026-03-15 15:17:18 +0000

    feat: generate users module with controller, service, and DTOs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [WorkspacesModule, TasksModule],
+  imports: [WorkspacesModule, TasksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
