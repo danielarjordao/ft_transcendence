702f4c8fdd9f66199c504e84ff3c56e1e4a36678
danielarjordao <danielarjordao@hotmail.com>
2026-03-24 11:14:02 +0000

    chore: add AuthModule to AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { FriendsModule } from './friends/friends.module';
 import { AccountModule } from './account/account.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [WorkspacesModule, TasksModule, UsersModule, FriendsModule, AccountModule],
+  imports: [WorkspacesModule, TasksModule, UsersModule, FriendsModule, AccountModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
