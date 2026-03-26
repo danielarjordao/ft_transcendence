de8b0494f67155626d88532e82419ebafda35f05
danielarjordao <danielarjordao@hotmail.com>
2026-03-26 21:21:29 +0000

    feat: add notifications and chat modules to app module imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,8 @@ import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { FieldsModule } from './fields/fields.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -21,6 +23,8 @@ import { FieldsModule } from './fields/fields.module';
     FriendsModule,
     AccountModule,
     AuthModule,
+    NotificationsModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
