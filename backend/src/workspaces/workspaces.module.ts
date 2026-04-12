93af2606094d52c8a0258283b0961895641c2d9a
danielarjordao <danielarjordao@hotmail.com>
2026-04-12 17:37:31 +0100

    feat(workspaces): integrate PrismaModule into WorkspacesModule

--- backend/src/workspaces/workspaces.module.ts
+++ backend/src/workspaces/workspaces.module.ts
@@ -1,10 +1,13 @@
+// workspaces.module.ts
 import { Module } from '@nestjs/common';
 import { WorkspacesController } from './workspaces.controller';
 import { WorkspacesService } from './workspaces.service';
 import { InvitationsController } from './invitations.controller';
 import { InvitationsService } from './invitations.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [WorkspacesController, InvitationsController],
   providers: [WorkspacesService, InvitationsService],
 })
