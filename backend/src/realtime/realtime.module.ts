aeb5a978a5650ab58eafd650e6a3d1999e647892
danielarjordao <danielarjordao@hotmail.com>
2026-04-28 19:51:47 +0100

    refactor(realtime): remove NotificationsGateway from providers in RealtimeModule

--- backend/src/realtime/realtime.module.ts
+++ backend/src/realtime/realtime.module.ts
@@ -2,7 +2,6 @@ import { Global, Module } from '@nestjs/common';
 import { AppGateway } from './app.gateway';
 import { ChatGateway } from './chat.gateway';
 import { WorkspacesGateway } from './workspaces.gateway';
-import { NotificationsGateway } from './notifications.gateway';
 import { ChatService } from '../chat/chat.service';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from '../prisma/prisma.module';
@@ -13,13 +12,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 @Global()
 @Module({
   imports: [PrismaModule, JwtModule.register({})],
-  providers: [
-    AppGateway,
-    ChatGateway,
-    WorkspacesGateway,
-    NotificationsGateway,
-    ChatService,
-  ],
+  providers: [AppGateway, ChatGateway, WorkspacesGateway, ChatService],
   // Exporting AppGateway allows other modules to inject it and emit events without needing to import the entire RealtimeModule.
   exports: [AppGateway],
 })
